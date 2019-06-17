88eb94179333543581d1b5871ee6057faa304c49
Alex Shumilov <ashumilov@pivotal.io>
2019-06-17 16:43:38 -0400

    Upgrade versions of libs used.

--- src/main/java/io/microsamples/generator/bootswag/api/DonutsApiController.java
+++ src/main/java/io/microsamples/generator/bootswag/api/DonutsApiController.java
@@ -2,20 +2,23 @@ package io.microsamples.generator.bootswag.api;
 
 
 import io.microsamples.generator.bootswag.model.Donut;
+import org.jeasy.random.EasyRandom;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static io.github.benas.randombeans.api.EnhancedRandom.randomStreamOf;
 
 @Controller
 public class DonutsApiController implements DonutsApi {
+
+    private EasyRandom easyRandom = new EasyRandom();
+
     @Override
     public ResponseEntity<List<Donut>> myDonuts() {
 
-        List<Donut> donuts = randomStreamOf(13, Donut.class).collect(Collectors.toList());
+        List<Donut> donuts = easyRandom.objects(Donut.class, 13).collect(Collectors.toList());
 
         return ResponseEntity.ok(donuts);
     }
